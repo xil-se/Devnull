da6920d01e58becfa98948a938fd4fc359945b15
Simon Vikstrom <sv@one.com>
2015-11-23 11:57:07 +0100

    less tabs better colors

--- MyEyeHurts_1/allthehack.php
+++ MyEyeHurts_1/allthehack.php
@@ -1,37 +1,36 @@
 <?php
 
-	$talksurl  = "http://halfnarp.events.ccc.de/-/talkpreferences?format=json";
-	$req_url   = "http://halfnarp.events.ccc.de/-/talkpreferences/public/%s?format=json";
-	$talks_array = json_decode(file_get_contents($talksurl), true);
+$talksurl  = "http://halfnarp.events.ccc.de/-/talkpreferences?format=json";
+$req_url   = "http://halfnarp.events.ccc.de/-/talkpreferences/public/%s?format=json";
+$talks_array = json_decode(file_get_contents($talksurl), true);
 
 
-	$pophashes = array();
+$pophashes = array();
 
-	$hashes = file("db.txt");
+$hashes = file("db.txt");
 
-	foreach($hashes as $hash){
-		$json = json_decode(file_get_contents(sprintf($req_url, trim($hash))), true);
-		$pophashes = array_merge($pophashes, $json["talk_ids"]);
-	}
+foreach($hashes as $hash){
+	$json = json_decode(file_get_contents(sprintf($req_url, trim($hash))), true);
+	$pophashes = array_merge($pophashes, $json["talk_ids"]);
+}
 
-	$pophashes = array_count_values($pophashes);
-	arsort($pophashes);
-
-
-	$maxval = array_values($pophashes)[0];
-	
-	foreach($pophashes as $k=>$v){
-		foreach($talks_array as $talk){
-			if($k == $talk["event_id"]){
+$pophashes = array_count_values($pophashes);
+arsort($pophashes);
 
 
-				$color = "rgb(0, ". (255-round(($v / $maxval) * 127)) . ", 0)";
-				echo "<div style='background-color:$color; color:#CCCCCC; font-weight:bold;'>";
-				echo $v, " : <i>", $talk["track_name"], "</i> -- ", $talk["title"], "\n";
-				echo "</div>";
-				continue;
-			}
+$maxval = array_values($pophashes)[0];
+
+foreach($pophashes as $k=>$v){
+	foreach($talks_array as $talk){
+		if($k == $talk["event_id"]){
+			$color = "rgb(0, ". (255-round(($v / $maxval) * 127)) . ", 0)";
+			$textcolor = "rgb(255, ". (255-(255-round(($v / $maxval) * 127))) . ", 255)";
+			echo "<div style='background-color:$color; color:$textcolor; font-weight:bold;'>";
+			echo $v, " : <i>", $talk["track_name"], "</i> -- ", $talk["title"], "\n";
+			echo "</div>";
+			continue;
 		}
 	}
+}
 
 ?>
