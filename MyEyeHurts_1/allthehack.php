a392bb3192ac96a6ec09ed9d72f423c6e0671d8e
Simon Vikstrom <sv@one.com>
2015-12-04 14:58:05 +0100

    uuuuuh

--- MyEyeHurts_1/allthehack.php
+++ MyEyeHurts_1/allthehack.php
@@ -11,6 +11,8 @@ $hashes = file("db.txt");
 
 
 $users = array();
+$usertalkmap = array();
+
 
 if(isset($_GET["users"]))
 	$users = explode(",", $_GET["users"]);
@@ -24,7 +26,8 @@ foreach($hashes as $hash){
 	}
 
 	$json = json_decode(file_get_contents(sprintf($req_url, trim($hasho))), true);
-	$pophashes = array_merge($pophashes, $json["talk_ids"]);
+	$usertalkmap[$user]  = $json["talk_ids"];
+	$pophashes = array_merge($pophashes, $usertalkmap[$user]);
 }
 
 $pophashes = array_count_values($pophashes);
@@ -36,15 +39,27 @@ $maxval = array_values($pophashes)[0];
 foreach($pophashes as $k=>$v){
 	foreach($talks_array as $talk){
 		if($k == $talk["event_id"]){
+
+			$agreeing = array();
+
+			foreach($usertalkmap as $user=> $values){
+				if(in_array($k, $values))
+					$agreeing[] = $user;
+			}
+			
+			
 			$color = "rgb(0, ". (255-round(($v / $maxval) * 127)) . ", 0)";
 			$textcolor = "rgb(255, ". (255-(255-round(($v / $maxval) * 127))) . ", 255)";
 			$textcolor = "#000";
 			echo "<div style='background-color:$color; color:$textcolor; font-weight:bold;'>";
-			echo $v, " : <i>", $talk["track_name"], "</i> -- ", $talk["title"], "\n";
+			echo $v, "(", implode(",", $agreeing), ") : <i>", $talk["track_name"], "</i> -- ", $talk["title"], "\n";
 			echo "</div>";
 			continue;
 		}
 	}
 }
+
+
+
 
 ?>
