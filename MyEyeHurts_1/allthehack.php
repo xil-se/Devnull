9242be47e8529457d33debd3f5aaaf7354f61aa7
Simon Vikstrom <sv@one.com>
2015-12-04 14:44:59 +0100

    Fixed stuff

--- MyEyeHurts_1/allthehack.php
+++ MyEyeHurts_1/allthehack.php
@@ -9,8 +9,21 @@ $pophashes = array();
 
 $hashes = file("db.txt");
 
+
+$users = array();
+
+if(isset($_GET["users"]))
+	$users = explode(",", $_GET["users"]);
+
 foreach($hashes as $hash){
-	$json = json_decode(file_get_contents(sprintf($req_url, trim($hash))), true);
+	list($user, $hasho) = explode(" ", $hash);
+
+	if(count($users) > 0){
+		if(!in_array($user, $users))
+			continue;
+	}
+
+	$json = json_decode(file_get_contents(sprintf($req_url, trim($hasho))), true);
 	$pophashes = array_merge($pophashes, $json["talk_ids"]);
 }
 
@@ -25,6 +38,7 @@ foreach($pophashes as $k=>$v){
 		if($k == $talk["event_id"]){
 			$color = "rgb(0, ". (255-round(($v / $maxval) * 127)) . ", 0)";
 			$textcolor = "rgb(255, ". (255-(255-round(($v / $maxval) * 127))) . ", 255)";
+			$textcolor = "#000";
 			echo "<div style='background-color:$color; color:$textcolor; font-weight:bold;'>";
 			echo $v, " : <i>", $talk["track_name"], "</i> -- ", $talk["title"], "\n";
 			echo "</div>";
